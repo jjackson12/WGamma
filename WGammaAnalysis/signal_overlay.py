ce2952e093950f95cf272ccf8707963302c80352
Jacob <jacob_jackson@brown.edu>
2018-04-12 00:47:47 -0400

    adding tau21

--- WGammaAnalysis/signal_overlay.py
+++ WGammaAnalysis/signal_overlay.py
@@ -22,6 +22,7 @@ histRanges["ph_phi"] = (-3.14,3.14)
 histRanges["jetAK8_eta"] = (-3.,3.)
 histRanges["jetAK8_phi"] = (-3.14,3.14)
 histRanges["jetAK8_puppi_softdrop_mass"] = (0,500)
+histRanges["tau12"] = (0,2.5)
 
 #TODO: 750 isn't among my signal MC; do I do 700 and/or 800?
 signalMasses = [600, 800,1000,2000,3500]
@@ -32,45 +33,58 @@ weightDict = getMCbgWeightsDict(bkgDir)
 
 
 
-observables = ["ph_pt","ph_eta","jetAK8_pt","jetAK8_eta","jetAK8_puppi_softdrop_mass"]
-
+observables = ["ph_pt","ph_eta","jetAK8_pt","jetAK8_eta","jetAK8_puppi_softdrop_mass","tau12"]
+observables = ["tau12"]
 
 for observable in observables:
 	print("plotting all for %s"%observable)
 
-	#Data
-	dataHist = TH1F("dataHist", "dataHist", nBins, histRanges[observable][0], histRanges[observable][1])
-	data.Draw("%s>>dataHist" % observable)
+	if(observable != "tau12"):
+		#Data
+		dataHist = TH1F("dataHist", "dataHist", nBins, histRanges[observable][0], histRanges[observable][1])
+
+		data.Draw("%s>>dataHist" % observable)
+
+	else:
+		#Data
+		dataHist = TH1F("dataHist", "dataHist", nBins, histRanges[observable][0], histRanges[observable][1])
+		dataHist2 = TH1F("dataHist2", "dataHist2", nBins, histRanges[observable][0], histRanges[observable][1])
+
+
+		data.Draw("%s>>dataHist" % "jetAK8_tau1")
+		data.Draw("%s>>dataHist2" % "jetAK8_tau2")
+		dataHist.Divide(dataHist2)
 
 	#Background
 	fileDict = {}
 	bkgHists = {}
 	iterTrees = {}
 
-	for bkgFile in os.listdir(bkgDir):
-	    print("processing background: %s\n" % bkgFile)
-	    bkg = re.search(r'(.*)smallified_(.*).root',bkgFile).groups()[1]
-	    fileDict[bkg] = TFile("%s/%s" % (bkgDir, bkgFile))
-	    iterTrees[bkg] = fileDict[bkg].Get("ntuplizer/tree")
-	    histLabel = "%s_%s" % (bkg, observable)
+	if(False):
 
-	    bkgHists[bkg]=TH1F(histLabel, histLabel, nBins, histRanges[observable][0], histRanges[observable][1])
-	    #iterTree.Draw('%s>>%s' % (observable, histLabel), str(weight))
-	    iterTrees[bkg].Draw('%s>>%s' % (observable, histLabel))
-	    efficiency = iterTrees[bkg].GetEntries()/fileDict[bkg].Get("ntuplizer/hCounter").GetBinContent(1)
-	    weight =  weightDict["%s.root"%bkg][0]
-	    bkgHists[bkg].Scale(weightDict["%s.root"%bkg][0])
-	    #bkgHists[bkg].Scale(weight)
-	    bkgHists[bkg].Sumw2()
+		for bkgFile in os.listdir(bkgDir):
+		    print("processing background: %s\n" % bkgFile)
+		    bkg = re.search(r'(.*)smallified_(.*).root',bkgFile).groups()[1]
+		    fileDict[bkg] = TFile("%s/%s" % (bkgDir, bkgFile))
+		    iterTrees[bkg] = fileDict[bkg].Get("ntuplizer/tree")
+		    histLabel = "%s_%s" % (bkg, observable)
+		    bkgHists[bkg]=TH1F(histLabel, histLabel, nBins, histRanges[observable][0], histRanges[observable][1])
+		    #iterTree.Draw('%s>>%s' % (observable, histLabel), str(weight))
+		    iterTrees[bkg].Draw('%s>>%s' % (observable, histLabel))
+		    efficiency = iterTrees[bkg].GetEntries()/fileDict[bkg].Get("ntuplizer/hCounter").GetBinContent(1)
+		    weight =  weightDict["%s.root"%bkg][0]
+		    bkgHists[bkg].Scale(weightDict["%s.root"%bkg][0])
+		    #bkgHists[bkg].Scale(weight)
+		    bkgHists[bkg].Sumw2()
 
-	histStack = THStack()
-	colors = [kRed,kBlue,kBlack,kGreen,kOrange,kYellow,kBlue-1,kRed+1,kGreen+2,kPink,kViolet,kAzure,kTeal,kTeal+3]
-	for bkgHist in bkgHists.itervalues():
-	    #bkgHist.Sumw2()
-	    color = colors.pop()
-	    bkgHist.SetLineColor(color)
-	    bkgHist.SetFillColor(color)
-	    histStack.Add(bkgHist)
+		histStack = THStack()
+		colors = [kRed,kBlue,kBlack,kGreen,kOrange,kYellow,kBlue-1,kRed+1,kGreen+2,kPink,kViolet,kAzure,kTeal,kTeal+3]
+		for bkgHist in bkgHists.itervalues():
+		    #bkgHist.Sumw2()
+		    color = colors.pop()
+		    bkgHist.SetLineColor(color)
+		    bkgHist.SetFillColor(color)
+		    histStack.Add(bkgHist)
 
 
 	#Signal
@@ -82,10 +96,16 @@ for observable in observables:
 	    fileDict[name] = TFile(fileName)
 	    iterTrees[name] = fileDict[name].Get("ntuplizer/tree")
 	    histLabel = "%s_%s" % (name, observable)
-
+		
 	    sigHists[name]=TH1F(histLabel, histLabel, nBins, histRanges[observable][0], histRanges[observable][1])
 	    #iterTree.Draw('%s>>%s' % (observable, histLabel), str(weight))
-	    iterTrees[name].Draw('%s>>%s' % (observable, histLabel))
+	    if(observable == "tau12"):
+	        sigHist2=TH1F("%s2"%histLabel, "%s2"%histLabel, nBins, histRanges[observable][0], histRanges[observable][1])
+	        iterTrees[name].Draw('%s>>%s' % ("jetAk8_tau1", histLabel))
+	        iterTrees[name].Draw('%s>>%s' % ("jetAK8_tau2", "%s2"%histLabel))
+	        sigHists[name].Divide(sigHist2)
+	    else:
+	        iterTrees[name].Draw('%s>>%s' % (observable, histLabel))
 	    sigHists[name].SetLineColor(kRed)
 	    return sigHists[name]
 
@@ -99,7 +119,10 @@ for observable in observables:
 	    cans[sigMass] = TCanvas("can","Signal overlay on Data",800,800)
 	    cans[sigMass].cd()
 	    signal = getSignalHist(sigMass)
-	    signal.Scale(dataHist.GetSumOfWeights()/signal.GetSumOfWeights())
+	    if(signal.GetSumOfWeights() != 0):
+	        signal.Scale(dataHist.GetSumOfWeights()/signal.GetSumOfWeights())
+	    else:
+	        print("WARNING: Zero sum of weights")
 	    signal.Draw()
 	    dataHist.Draw("SAME")
 	    leg = TLegend(0.7,0.7,0.9,0.9)
@@ -114,3 +137,4 @@ for observable in observables:
 	    outFile.cd()
 	    cans[sigMass].Write()
 	    outFile.Close()
+
